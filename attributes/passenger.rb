20a6f1ce39ca324d076db275f78a98e811af495b
Jose Alberto <ja@josealberto.org>
2013-11-19 16:41:19 +0000

    add vhost template & fixed passenger conf file

--- attributes/passenger.rb
+++ attributes/passenger.rb
@@ -1,16 +1,6 @@
-node.default['passenger_nginx_repo']['version'] = '4.0.20'
-
-if node['languages'].attribute?('ruby')
-  node.default['passenger_nginx_repo']['root'] = "#{node['languages']['ruby']['gems_dir']}/gems/passenger-#{node['passenger_nginx_repo']['version']}"
-  node.default['passenger_nginx_repo']['ruby'] = node['languages']['ruby']['ruby_bin']
-else
-  Chef::Log.warn("node['languages']['ruby'] attribute not detected in #{cookbook_name}::#{recipe_name}")
-  Chef::Log.warn("Install a Ruby for automatic detection of node['nginx']['passenger'] attributes (root, ruby)")
-  Chef::Log.warn('Using default values that may or may not work for this system.')
-  node.default['passenger_nginx_repo']['root'] = "/usr/lib/ruby/gems/1.8/gems/passenger-#{node['nginx']['passenger']['version']}"
-  node.default['passenger_nginx_repo']['ruby'] = '/usr/bin/ruby'
-end
-
+# node.default['passenger_nginx_repo']['version']               = '4.0.20'
+node.default['passenger_nginx_repo']['root']                  = "/usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini"
+node.default['passenger_nginx_repo']['ruby']                  = node['rvm']['root_path'] + '/rubies/default'
 node.default['passenger_nginx_repo']['spawn_method']          = 'smart-lv2'
 node.default['passenger_nginx_repo']['buffer_response']       = 'on'
 node.default['passenger_nginx_repo']['max_pool_size']         = 6
--- recipes/passenger.rb
+++ recipes/passenger.rb
@@ -18,3 +18,23 @@ template "/etc/nginx/conf.d/passenger.conf" do
   mode   '0644'
   notifies :reload, 'service[nginx]'
 end
+
+node['passenger_nginx_repo']['apps'].each do |app|
+  template( File.join(node["nginx"]["dir"], "sites-available", app["appname"]) ) do
+    source 'nginx_vhost.conf.erb'
+    owner  'root'
+    group  'root'
+    mode   '0644'
+    variables(
+      root_path:   ::File.join(app['app_dir'], 'public'),
+      log_dir:     node["nginx"]["log_dir"],
+      appname:     app['appname'],
+      hostname:    app['hostname'],
+      listen_port: app['listen_port'],
+      ssl_key:     app['ssl_key'],
+      ssl_cert:    app['ssl_cert'],
+      dev_mode:    app['dev_mode']
+    )
+    notifies :reload, 'service[nginx]'
+  end
+end
