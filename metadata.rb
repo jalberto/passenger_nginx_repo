30e95b9d62e4cbe32b5e3d3ba867f0ac2df884de
Jose Alberto <ja@josealberto.org>
2013-11-25 10:49:19 +0000

    remove depends

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs passenger with nginx from official package'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'apt'
+# depends 'apt'
 
