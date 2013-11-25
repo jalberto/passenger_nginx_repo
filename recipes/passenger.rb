0d8dcdc03a7c74320869ce44b40e8e9bb8770dcd
Jose Alberto <ja@josealberto.org>
2013-11-25 15:45:16 +0000

    improved enabling site

--- recipes/passenger.rb
+++ recipes/passenger.rb
@@ -39,8 +39,9 @@ node['passenger_nginx_repo']['apps'].each do |app|
     )
     notifies :reload, 'service[nginx]'
   end
-  execute "enable vhost" do
-    command "ln -s #{File.join(node['nginx']['dir'], 'sites-available', app['appname'])} #{File.join(node['nginx']['dir'], 'sites-enabled', app['appname'])}"
+
+  link "#{node['nginx']['dir']}/sites-enabled/#{app['appname']}" do
+    to "#{node['nginx']['dir']}/sites-available/#{app['appname']}"
     not_if { ::File.symlink?(File.join(node['nginx']['dir'], 'sites-enabled', app['appname'])) }
     only_if { ::File.exists?(File.join(node['nginx']['dir'], 'sites-available', app['appname'])) }
   end
