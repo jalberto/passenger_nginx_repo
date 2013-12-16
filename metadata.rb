992d3788220f031012e7f122caf29402e8b93128
paulmwatson <github.com@paulmwatson.com>
2013-12-16 11:51:43 +0000

    Do not add second host option to nginx in dev mode so that assets can be served

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ja@josealberto.org'
 license          'MIT'
 description      'Installs passenger with nginx from official package'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 # depends 'apt'
 
